3341a9b296b91b2e60e480a64ea5dbc6c7b47dd3
Evgenii Saganchi <esaganchi@gmail.com>
2025-12-11 18:36:58 +0100

    add article creating test

--- tests/postAPIExample.spec.ts
+++ tests/postAPIExample.spec.ts
@@ -10,18 +10,23 @@ test('Create a new Article', async ({ request }) => {
         }
     })
     const tokenResponseJSON = await tokenResponse.json()
-    const authToken = tokenResponseJSON.user.token
-    console.log(authToken)
+    const authToken = 'Token ' + tokenResponseJSON.user.token
 
+    const uniqueTitle = `Test Article Saganchi ${Date.now()}`
+    const newArticleResponse = await request.post('https://conduit-api.bondaracademy.com/api/articles', {
+        data: {
+            article: {
+                title: uniqueTitle,
+                description: 'Test Description Saganchi',
+                body: 'Test Body Saganchi',
+                tagList: ['Test', 'Test2', 'Test3']
+            }
+        },
+        headers: {
+            'Authorization': authToken
+        }
+    })
+    const newArticleResponseJSON = await newArticleResponse.json()
+    expect(newArticleResponse.status()).toBe(201)
 
-
-
-//   const tagsResponse = await request.get('https://conduit-api.bondaracademy.com/api/tags')
-//   const tagsResponseJSON = await tagsResponse.json()
-
-//   expect(tagsResponse.status()).toBe(200)
-//   expect(tagsResponseJSON.tags[0]).toBe('Test')
-//   expect(tagsResponseJSON.tags.length).toBeLessThanOrEqual(10)
-
-//   console.log(tagsResponseJSON)
 });
