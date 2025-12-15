85c7a55a4bde7d456a8e5235d9a1f21220fbc746
Evgenii Saganchi <esaganchi@gmail.com>
2025-12-15 23:04:29 +0100

    30_request_handler_constructor

--- utils/request-handler.ts
+++ utils/request-handler.ts
@@ -1,45 +1,50 @@
+import { APIRequestContext } from '@playwright/test';
+
 export class RequestHandler {
+  private request: APIRequestContext;
+  private baseUrl: string;
+  private defaultBaseUrl: string;
+  private apiPath: string = '';
+  private queryParams: object = {};
+  private apiHeaders: object = {};
+  private apiBody: object = {};
 
-    private baseUrl: string
-    private defaultBaseUrl: string = 'https://conduit-api.bondaracademy.com/api'
-    private apiPath: string = ''
-    private queryParams: object = {}
-    private apiHeaders: object = {}
-    private apiBody: object = {}
-
-
-    url(url: string) {
-        this.baseUrl = url
-        return this
-    }
-  
-    path(path: string) {
-        this.apiPath = path
-        return this
-    }
-  
-    params(params: object) {
-        this.queryParams = params
-        return this
-    }
-  
-    headers(headers: object) {
-        this.apiHeaders = headers
-        console.log(this.apiHeaders)
-        return this
-    }
-  
-    body(body: object) {
-        this.apiBody = body
-        return this
-    }
-
-    private getUrl() {
-        const url = new URL(`${this.baseUrl ?? this.defaultBaseUrl}${this.apiPath}`);
-        for (const [key, value] of Object.entries(this.queryParams)) {
-          url.searchParams.append(key, value);
-        }
-        return url.toString();
-      }      
-
+  constructor(request: APIRequestContext, apiBaseUrl: string) {
+    this.request = request;
+    this.defaultBaseUrl = apiBaseUrl;
   }
+
+  url(url: string) {
+    this.baseUrl = url;
+    return this;
+  }
+
+  path(path: string) {
+    this.apiPath = path;
+    return this;
+  }
+
+  params(params: object) {
+    this.queryParams = params;
+    return this;
+  }
+
+  headers(headers: object) {
+    this.apiHeaders = headers;
+    console.log(this.apiHeaders);
+    return this;
+  }
+
+  body(body: object) {
+    this.apiBody = body;
+    return this;
+  }
+
+  private getUrl() {
+    const url = new URL(`${this.baseUrl ?? this.defaultBaseUrl}${this.apiPath}`);
+    for (const [key, value] of Object.entries(this.queryParams)) {
+      url.searchParams.append(key, String(value));
+    }
+    return url.toString();
+  }
+}
