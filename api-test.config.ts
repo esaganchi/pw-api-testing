134ed4adeb51007e0de709887c7c4b1136cefa2b
Evgenii Saganchi <esaganchi@gmail.com>
2025-12-21 22:15:55 +0100

    50_added_request_object

--- api-test.config.ts
+++ api-test.config.ts
@@ -1,5 +1,5 @@
 const processENV = process.env.TEST_ENV;
-const env = processENV || 'qa';
+const env = processENV || 'prod';
 console.log('Test environment is: ' + env);
 
 const config = {
@@ -10,9 +10,6 @@ const config = {
 
 if (env === 'qa') {
     ((config.userEmail = 'pwtest@test.com'), (config.userPassword = 'Welcome2'));
-}
-if (env === 'prod') {
-    ((config.userEmail = 'saga1993@gmail.com'), (config.userPassword = 'saga1993'));
 }
 
 export { config };
--- tests/smokeTest.spec.ts
+++ tests/smokeTest.spec.ts
@@ -2,6 +2,7 @@ import { test } from '../utils/fixtures';
 import { expect } from '@playwright/test';
 import { expect as customExpect } from '../utils/customExpect';
 import { validateSchema } from '../utils/schema-validator';
+import articleRequestPayload from '../request-objects/POST-article.json';
 
 test('Get Articles', async ({ api }) => {
     const response = await api
@@ -22,33 +23,29 @@ test('Get Test Tags', async ({ api }) => {
 });
 
 test('Create and Delete Article', async ({ api }) => {
-    const uniqueTitle = `Test TWO Test ${Date.now()}`;
+    const articleRequest = JSON.parse(JSON.stringify(articleRequestPayload));
+    articleRequest.article.title = `This is a test title`;
     const createArticleResponse = await api
         .path('/articles')
-        .body({
-            article: {
-                title: uniqueTitle,
-                description: 'Test TWO Description',
-                body: 'Test TWO Body',
-                tagList: ['Test TWO'],
-            },
-        })
+        .body(articleRequest)
         .postRequest(201);
     await expect(createArticleResponse).shouldMatchSchema('articles', 'POST_article');
-    customExpect(createArticleResponse.article.title).shouldEqual(uniqueTitle);
-    const slug = createArticleResponse.article.slug;
+    expect(createArticleResponse.article.title).shouldEqual(articleRequestPayload.article.title);
+    const slugId = createArticleResponse.article.slug;
 
-    // Получаем конкретную статью по slug вместо списка, чтобы избежать проблем с параллельными тестами
-    const articleResponse = await api.path(`/articles/${slug}`).getRequest(200);
-    customExpect(articleResponse.article.title).shouldEqual(uniqueTitle);
-
-    await api.path(`/articles/${slug}`).deleteRequest(204);
-
-    const articleResponseAfter = await api
-        .path('/articles/')
+    const articlesResponse = await api
+        .path('/articles')
         .params({ limit: 10, offset: 0 })
         .getRequest(200);
-    customExpect(articleResponseAfter.articles[0].title).not.shouldEqual(uniqueTitle);
+    expect(articlesResponse.articles[0].title).shouldEqual(articleRequestPayload.article.title);
+
+    await api.path(`/articles/${slugId}`).deleteRequest(204);
+
+    const articlesResponseTwo = await api
+        .path('/articles')
+        .params({ limit: 10, offset: 0 })
+        .getRequest(200);
+    expect(articlesResponseTwo.articles[0].title).not.shouldEqual('Test TWO TEST');
 });
 
 test('Create, Update and Delete Article', async ({ api }) => {
